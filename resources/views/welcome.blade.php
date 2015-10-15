78002db28552668def1c4c0f322551e40106ce1c
Davide Bellini <bellini.davide@gmail.com>
2015-10-15 19:17:20 +0200

    Piccole migliorie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,63 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title>{{ config('website.name') }}</title>
 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="description" content="Meetup a Verona legati a Laravel e PHP">
+        <meta name="keywords" content="laravel, php, framework, web, meetup, verona">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-                background: url('images/pattern.png');
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                color: #DA402D;
-                font-size: 70px;
-                margin-bottom: 20px;
-                padding-bottom: 20px;
-                border-bottom: 1px dotted #ccc;
-            }
-
-            .subtitle {
-                font-size: 40px;
-                margin-bottom: 40px;
-            }
-
-            .button {
-                color: #fff;
-                margin-top: 40px;
-                padding: 15px 30px;
-                text-decoration: none;
-                text-transform: uppercase;
-                background-color: #DA402D;
-            }
-            .button:hover,
-            .button:focus,
-            .button:active {
-                background-color: #D93B36;
-            }
-        </style>
+        <link href="css/website.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="container">
@@ -65,19 +18,18 @@
                 <div class="title">1° Laravel Meetup a Verona</div>
                 <div class="subtitle">14 novembre 2015</div>
 
-                <a class="button" rel="nofollow" href="http://www.meetup.com/it/laravel-verona/">Unisciti a noi</a>
+                <a class="button" href="http://www.meetup.com/it/laravel-verona/">Unisciti a noi</a>
             </div>
         </div>
 
         <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-8557461-13', 'auto');
-          ga('send', 'pageview');
-
+            ga('create', 'UA-8557461-13', 'auto');
+            ga('send', 'pageview');
         </script>
     </body>
 </html>
